cf1a35479fde0c0b880cc36898b315bef2c2e922
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2024-02-13 13:01:35 +0300

    implemented the download

--- src/aws/utils.py
+++ src/aws/utils.py
@@ -2,6 +2,7 @@ import logging
 
 from src.aws.client import client
 from src.config import AWS_BUCKET
+from botocore.exceptions import ClientError
 
 
 async def s3_upload(contents: bytes, key: str) -> None:
@@ -14,3 +15,12 @@ async def s3_upload(contents: bytes, key: str) -> None:
         logging.info(f'{key} successfully uploaded to S3')
     except Exception as e:
         logging.error(f'Error uploading {key} to S3: {str(e)}')
+
+
+async def s3_download(key: str) -> bytes:
+    logging.info(f'Downloading {key} from s3...')
+    try:
+        response = client.get_object(Bucket=AWS_BUCKET, Key=key)
+        return response['Body'].read()
+    except ClientError as err:
+        logging.error(str(err))
