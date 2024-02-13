a7928f855ce6348dd36a30da2c466c2160126b96
v.riabikin <ryabickin.vlad@ya.ru>
2024-02-13 13:18:22 +0300

    auth base config updated

--- src/auth/base_config.py
+++ src/auth/base_config.py
@@ -1,7 +1,10 @@
+from fastapi_users import FastAPIUsers
 from fastapi_users.authentication import AuthenticationBackend, BearerTransport
 from fastapi_users.authentication import JWTStrategy
 
 from config import SECRET_AUTH
+from auth.manager import get_user_manager
+from auth.models import User
 
 bearer_transport = BearerTransport(tokenUrl="api/auth/jwt/login")
 
@@ -14,4 +17,9 @@ auth_backend = AuthenticationBackend(
     name="jwt",
     transport=bearer_transport,
     get_strategy=get_jwt_strategy,
+)
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
 )
