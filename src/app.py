a6f4f9936cd2985cf57eda4068b358f6c6a4cd9c
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2024-02-13 14:04:26 +0300

    add CORS

--- src/app.py
+++ src/app.py
@@ -1,3 +1,21 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+import message.router as chat_router
+from auth.base_config import fastapi_users
+from router import router
 
 app = FastAPI(title="PolyTex WebChat", version="0.0.1")
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
